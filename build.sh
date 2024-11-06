f166f0a6bb4f56e0b0f67318bdb720593f66879a
omihalcea <omihalcea@iesebre.com>
2024-11-06 19:37:11 +0100

    install tailwind

--- build.sh
+++ build.sh
@@ -10,3 +10,6 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+python manage.py tailwind install
+python manage.py tailwind init
+
